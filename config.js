4268b2635f42d77387d28c7f409a7bdbc45bf41a
DominicDev <s0l0estudi4r@gmail.com>
2026-02-06 13:54:28 +0100

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'NOISESTORM';
+export let nameSongs = [
+	"CRAB RAVE", /*1*/
+	"BREAKDOWN", /*2*/
+	"HEIST", /*3*/
+	"IGNITE", /*4*/
+	"SOLAR", /*5*/
+	"SHOCKWAVE", /*6*/
+	"INTENSITY", /*7*/
+	"IMPULSE", /*8*/
+	"AFTERBURNER", /*9*/
+	"RENEGADE", /*10*/
+	"WIPEOUT", /*11*/
+	"BACKLASH", /*12*/
+	"TOGETHER", /*13*/
+	"SUB ZERO", /*14*/
+	"LET IT ROAR", /*15*/
+	"AIRWAVES", /*16*/
+	"PULSE", /*17*/
+	"SURVIVAL", /*18*/
+	"PANORAMIC", /*19*/
+	"FULL FOCUS", /*20*/
+]
